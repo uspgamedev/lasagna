aa4c6564b7956d50cb0a7065b39fcf201fd587c5
Kazuo256 <kazuo256@gmail.com>
2018-04-21 16:48:49 -0300

    Health bar sprite

--- game/hud/status/healthbar/healthbar.gd
+++ game/hud/status/healthbar/healthbar.gd
@@ -1,5 +1,10 @@
 extends ProgressBar
 
 func change(amount):
-	$Tween.interpolate_property(self, "value", self.value, self.value + amount, 1, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	$Tween.interpolate_method(self, "_set_amount", self.value, self.value + amount, 3, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	$Tween.start()
+
+func _set_amount(amount):
+	self.value = amount
+	var color = Color(.8, .1, .1).linear_interpolate(Color(.1, .8, .1), amount/100.0)
+	get_stylebox("fg", "").modulate_color = color
