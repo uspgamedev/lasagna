a89311cd916b585ce9eff35344d240ae1e4bb98b
josealvim <jose.alvim@usp.com>
2018-04-21 15:14:51 -0300

    fix recipe logic

--- game/hud/menu/crafting/recipe.gd
+++ game/hud/menu/crafting/recipe.gd
@@ -3,19 +3,19 @@ extends Panel
 export (int) var ingred_num = 1
 
 func _ready():
-	for x in range(6-ingred_num):
+	for x in range(ingred_num, 6):
 		var nodename = "HBox/Ingredient"+str(x)
-		get_node(nodename).visibility = false
+		get_node(nodename).visible = false
 	pass
 
 func _process(delta):
 	var can_craft = true
-	for x in range(6-ingred_num, 6):
+	for x in range(0,ingred_num):
 		var nodename = "HBox/Ingredient"+str(x)
 		can_craft = can_craft and get_node(nodename).has_enough()
 		if !can_craft:
 			break
 	if can_craft:
-		$CraftButton.disable = false
+		$CraftButton.disabled = false
 	else:
-		$CraftButton.disable = true
+		$CraftButton.disabled = true
