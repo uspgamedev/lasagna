8c2371ec971b797956dfbbd0f0b8eacfc101431c
IceMage144 <jgabriel.basi@hotmail.com>
2018-04-21 19:52:32 -0300

    Fix cutscene bug

--- game/character/player/input/input_node.gd
+++ game/character/player/input/input_node.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+onready var Time = get_node("Timer")
+
 var direction = Vector2(0,0)
 var prev_input = false
 signal interact
@@ -13,12 +15,15 @@ func current_direction():
 	return direction.normalized()
 	
 func prevent_input(b):
+	if not b:
+		Time.start()
+		yield(Time, "timeout")
 	prev_input = b
 
 func _process(delta):
 	direction = Vector2(0, 0)
 	if not prev_input:
-		if Input.is_action_just_pressed("ui_interact"):
+		if Input.is_action_just_pressed("ui_accept"):
 			emit_signal('interact')
 		elif Input.is_action_pressed("ui_up"):
 			direction += UP
@@ -29,5 +34,4 @@ func _process(delta):
 		elif Input.is_action_pressed("ui_right"):
 			direction += RT
 
-func _unhandled_key_input(event):
 	
