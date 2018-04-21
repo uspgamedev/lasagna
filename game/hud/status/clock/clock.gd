2e44db42892dfa93ed641d2c0278841d20661d6a
josealvim <jose.alvim@usp.com>
2018-04-21 12:50:44 -0300

    make the clock a progress bar rather than analogic with animation

--- game/hud/status/clock/clock.gd
+++ game/hud/status/clock/clock.gd
@@ -3,26 +3,15 @@ extends Node
 signal midnight_strikes
 
 export (int) var minute_frames
-export (int) var   hour_frames
-
-var minute_hand_pos
-var   hour_hand_pos
-
-func _ready():
-	minute_hand_pos = 0
-	hour_hand_pos   = 0
 
 func end_night():
-	start()
+	$Timer.start()
 	pass
 
 func _process(delta):
-	var ratio       = $Timer.time_left/$Timer.wait_time
-	hour_hand_pos   =  int(hour_frames*ratio)
-	minute_hand_pos = int(ratio*minute_frames*12)%12
-	
-	$MinuteHand.frame = minute_hand_pos
-	$HourHand.frame   = hour_hand_pos
+	var ratio = $Timer.time_left/$Timer.wait_time
+	var bar   = $DayNightBar
+	bar.value = bar.max_value*ratio 
 	
 	if ratio == 1:
 		emit_signal("midnight_strikes")
