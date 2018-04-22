aef82a591952af51a229921d06196033484fbf2b
Arthur Vieira Barbosa <arthur.vb@gmail.com>
2018-04-22 18:15:34 -0300

    blood moon functionality

--- game/hud/status/clock/clock.gd
+++ game/hud/status/clock/clock.gd
@@ -2,47 +2,27 @@ extends Control
 
 signal midnight_strikes
 
-enum MOON {
-	FULL,
-	CRESCENT,
-	NEW,
-	WANING,
-	BLOOD
-}
-
 export (int) var day_duration = 10 # duration of daytime in seconds
 
-var day = 1
-var moon = 1
+var flags
 
 func _ready():
 	$Timer.set_wait_time(day_duration)
 	$Timer.start()
-	moon = day % 4
+	flags = get_node("/root/Main").get_flags()
 
 func end_night():
 	$Timer.start()
-	day += 1
-	if moon == FULL:
-		# try blood moon
-		pass
 
 func get_daytime():
 	return 1 - $Timer.time_left/$Timer.wait_time
 
 func get_day():
-	return day
-
-func get_moon():
-	match moon:
-		NEW: return "New"
-		CRESCENT: return "Crescent"
-		FULL: return "Full"
-		WANING: return "Waning"
-		BLOOD: return "Blood"
+	return flags.get_day()
 
 func _process(delta):
 	var ratio = $Timer.time_left/$Timer.wait_time
+	var moon = flags.get_moon()
 	
 	var rect = $DayNightBar.get_region_rect()
 	rect.position.x = 256 * moon + 128 * (1 - ratio)
