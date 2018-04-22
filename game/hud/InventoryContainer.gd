6b3e0403ff6adbd12b784a4824694a91bfcf7d2c
Victor Domiciano <victor.dm3@gmail.com>
2018-04-22 16:50:22 -0300

    Add transition between highlighted slot in stash and inventory

--- game/hud/InventoryContainer.gd
+++ game/hud/InventoryContainer.gd
@@ -2,6 +2,7 @@ extends GridContainer
 
 onready var slots = get_children()
 onready var sprite = get_node('MenuButton1/Sprite')
+onready var inventory = get_node('../Status/Bottom/Inventory')
 var actual_slot = 1
 
 func _ready():
@@ -24,17 +25,29 @@ func render_inventory():
 		button.icon = load("res://test/empy-slot.png")
 
 func move_highlighted_slot(new_slot):
-	get_node(str('MenuButton' + var2str(actual_slot))).remove_child(sprite)
-	get_node(str('MenuButton' + var2str(new_slot))).add_child(sprite)
+	if (actual_slot <= 8 and actual_slot >= 1):
+		get_node(str('MenuButton' + var2str(actual_slot))).remove_child(sprite)
+	else:
+		inventory.get_node(str('Slot' + var2str(actual_slot - 8))).remove_child(sprite)
+	if (new_slot <= 8 and new_slot >= 1):
+		sprite.offset = Vector2(22, 19)
+		get_node(str('MenuButton' + var2str(new_slot))).add_child(sprite)
+	else:
+		sprite.offset = Vector2(16, 16)
+		inventory.get_node(str('Slot' + var2str(new_slot - 8))).add_child(sprite)
 	actual_slot = new_slot
 
 func _input(event):
 	if (self.is_visible_in_tree()):
-		if event.is_action_pressed('ui_right') and actual_slot != 4:
-				move_highlighted_slot(min(actual_slot + 1, 8))
-		elif event.is_action_pressed('ui_left') and actual_slot != 5:
-			move_highlighted_slot(max(actual_slot - 1, 1))
-		elif event.is_action_pressed('ui_up') and actual_slot - 4 >= 1:
+		if event.is_action_pressed('ui_right') and actual_slot != 4 and actual_slot != 8 and actual_slot != 14:
+			move_highlighted_slot(actual_slot + 1)
+		elif event.is_action_pressed('ui_left') and actual_slot != 1 and actual_slot != 5 and actual_slot != 9:
+			move_highlighted_slot(actual_slot - 1)
+		elif event.is_action_pressed('ui_up'):
+			if actual_slot <= 12 and actual_slot >= 5:
 				move_highlighted_slot(actual_slot - 4)
-		elif event.is_action_pressed('ui_down') and actual_slot + 4 <= 8:
+			elif actual_slot == 13 or actual_slot == 14:
+				move_highlighted_slot(8)
+		elif event.is_action_pressed('ui_down'):
+			if actual_slot <= 8 and actual_slot >= 1:
 				move_highlighted_slot(actual_slot + 4)
