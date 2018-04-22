f8242d811278495e6ac601dfbc5d3bcd95e40e1e
Victor Domiciano <victor.dm3@gmail.com>
2018-04-22 18:09:57 -0300

    Hide inventory cursor during stash interaction

--- game/objects/stash/StashInteraction.gd
+++ game/objects/stash/StashInteraction.gd
@@ -3,23 +3,22 @@ extends Node
 func _input(event):
 	var hud = get_node("/root/Main").get_hud()
 	var container = hud.get_node("InventoryContainer")
+	var inventory = hud.get_node('Status/Bottom/Inventory')
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().paused = false
 		container.hide()
+		inventory.slots[inventory.focused].focus()
 		if container.actual_slot >= 9:
-			hud.get_node(str('Status/Bottom/Inventory/Slot' + var2str(container.actual_slot - 8) + '/Sprite')).visible = false
+			inventory.get_node(str('Slot' + var2str(container.actual_slot - 8) + '/Sprite')).visible = false
 
 func interact():
 	var hud = get_node("/root/Main").get_hud()
 	var container = hud.get_node("InventoryContainer")
+	var inventory = hud.get_node('Status/Bottom/Inventory')
 	container.render_inventory()
 	container.show()
+	inventory.slots[inventory.focused].unfocus()
 	if container.actual_slot >= 9:
-		hud.get_node(str('Status/Bottom/Inventory/Slot' + var2str(container.actual_slot - 8) + '/Sprite')).visible = true
+		inventory.get_node(str('Slot' + var2str(container.actual_slot - 8) + '/Sprite')).visible = true
 	container.move_highlighted_slot(1)
 	get_tree().paused = true
-
-	# Abrir menu de interacao
-	
-	# com esse menu, ser possivel coletar itens do bau (e move-los pro inventario), colocar
-	# itens do inventario no bau, e consultar o seu dinheiro(?)
