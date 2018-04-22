eb47873926fdde00b7faea149a9ab5582c19ac54
Detril <andremouka@hotmail.com>
2018-04-22 16:11:28 -0300

    Player's interaction now detects plantable ground tile @Detril rulez

--- game/character/player/interaction/AreaInteraction.gd
+++ game/character/player/interaction/AreaInteraction.gd
@@ -1,19 +1,16 @@
 extends Area2D
 
-onready var player_sprite = get_node('../OrientedSprite')
-onready var polygon = get_node('CollisionPolygon2D')
-
-var dirs = [PoolVector2Array([Vector2(-12, 16), Vector2(-12, 48), Vector2(12, 48), Vector2(12, 16)]),
-			PoolVector2Array([Vector2(-36, -16), Vector2(-36, 16), Vector2(-12, 16), Vector2(-12, -16)]),
-			PoolVector2Array([Vector2(-12, -48), Vector2(-12, -16), Vector2(12, -16), Vector2(12, -48)]),
-			PoolVector2Array([Vector2(36, -16), Vector2(36, 16), Vector2(12, 16), Vector2(12, -16)])]
+var dirs = [Vector2(32, 0),
+			Vector2(-32, 0),
+			Vector2(0, 16),
+			Vector2(0, -16)]
 
 func rotate_polygon_collision(direction):
 	if direction == 'up':
-		polygon.polygon = dirs[3]
+		set_position(dirs[3])
 	elif direction == 'down':
-		polygon.polygon = dirs[1]
+		set_position(dirs[2])
 	elif direction == 'left':
-		polygon.polygon = dirs[2]
+		set_position(dirs[1])
 	elif direction == 'right':
-		polygon.polygon = dirs[0]
+		set_position(dirs[0])
--- game/character/player/player.gd
+++ game/character/player/player.gd
@@ -6,12 +6,21 @@ func _process(delta):
 
 func _on_InputNode_interact():
 	var area = get_node('AreaInteraction')
+	
 	for body in area.get_overlapping_bodies():
 		if body != self:
 			for child in body.get_children():
 				if child.get_name() == 'Interaction':
 					child.interact()
 					return
+	
+	var g_pos = area.get_global_position()
+	var floor_tilemap = get_parent().get_parent().get_node("Background")
+	var ft_pos = floor_tilemap.world_to_map(g_pos)
+	var cell = floor_tilemap.get_cellv(ft_pos)
+	
+	if cell == 1:
+		print("É HORA DA COLHEITA")
 
 func has_interactive_node(vec):
 	for node in vec:
