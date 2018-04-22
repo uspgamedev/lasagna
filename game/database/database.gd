30c8f229d7f416b3ab17515d2f7ed312b72af375
josealvim <jose.alvim@usp.com>
2018-04-22 11:06:59 -0300

    add script for database interface

---
+++ game/database/database.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func get_item_by_name(godot_name):
+	return $Items.get_node(godot_name)
+	
+func get_recipe_by_name(godot_name):
+	return $Recipies.get_node(godot_name)
+
+func get_recipe_ingreds(recipe):
+	var array = {}
+	for ingred_req in recipe.get_children():
+		array.append(get_item_by_name(ingred_req.name))
+	return array
