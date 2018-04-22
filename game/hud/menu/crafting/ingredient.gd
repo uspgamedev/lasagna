6466d1ae3b492ec87730c89e83d4f101ce672ad6
josealvim <jose.alvim@usp.com>
2018-04-22 11:48:50 -0300

    implement inventory checking for crafting (WIP)

--- game/hud/menu/crafting/ingredient.gd
+++ game/hud/menu/crafting/ingredient.gd
@@ -3,13 +3,17 @@ extends Control
 export (Texture) var ingred_texture
 export (int)     var amount
 
+var ingred_name = ""
+
 func _ready():
 	$Button.icon        = ingred_texture
 	$Button/Amount.text = str(amount)+"x"
 
 func has_enough():
-	#IF PLAYER HAS ENOUGH, TRUE; ELSE FALSE
-	pass
+	#var player = ...
+	var inventory = player.get_node("Inventory")
+	var owned = inventory.how_many_of_item(ingred_name) 
+	return owned >= amount
 
 func _process(delta):
 	if ingred_texture != $Button.icon:
