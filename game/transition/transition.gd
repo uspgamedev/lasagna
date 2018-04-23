a86449768073feb1f6e394dd69613ac1fe8e2bf8
IceMage144 <jgabriel.basi@hotmail.com>
2018-04-23 01:41:54 -0300

    Add garden scene and doors

--- game/transition/transition.gd
+++ game/transition/transition.gd
@@ -1,17 +1,27 @@
 extends CanvasLayer
 
-export (int) var transition_duration = 2
+signal got_dark
 
-func _ready():
-	$Timer.set_wait_time(transition_duration)
-
-func play():
+func play(type, duration=2):
+	$Timer.set_wait_time(duration)
+	match type:
+		"morning":
+			$Scenery.set_frame(5)
+		"night":
+			match get_node("/root/Main").get_flags().get_moon_name():
+				"Full": $Scenery.set_frame(0)
+				"Crescent": $Scenery.set_frame(1)
+				"Waning": $Scenery.set_frame(2)
+				"New": $Scenery.set_frame(3)
+				"Blood": $Scenery.set_frame(4)
+		"scene":
+			$Scenery.modulate = Color(0, 0, 0)
 	get_node("/root/Main").pause_clock()
 	$AnimationPlayer.play("transition_in")
 
 func transition_in_ended():
 	$Timer.start()
-	
+	emit_signal("got_dark")
 	# Things to be changed behind the scenes should be done here
 	get_node("/root/Main").update_clock()
 
@@ -21,15 +31,3 @@ func _on_Timer_timeout():
 func transition_out_ended():
 	get_node("/root/Main").unpause_clock()
 	queue_free()
-
-func is_morning(morning):
-	if morning:
-		$Scenery.set_frame(5)
-		return
-	
-	match get_node("/root/Main").get_flags().get_moon_name():
-		"Full": $Scenery.set_frame(0)
-		"Crescent": $Scenery.set_frame(1)
-		"Waning": $Scenery.set_frame(2)
-		"New": $Scenery.set_frame(3)
-		"Blood": $Scenery.set_frame(4)
