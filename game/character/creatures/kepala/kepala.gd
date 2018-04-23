38ca40de12e29392d08a1ece999d31dc9fe4e526
IceMage144 <jgabriel.basi@hotmail.com>
2018-04-23 06:21:26 -0300

    Slow down kepala's attack

--- game/character/creatures/kepala/kepala.gd
+++ game/character/creatures/kepala/kepala.gd
@@ -6,8 +6,6 @@ var attacked = false
 func _process(delta):
 	if attacking and not attacked:
 		for node in $Area2D.get_overlapping_bodies():
-			print("LOOPPED")
 			if node.name == "Player":
-				print("ATTACKED")
 				node.damage(10)
 				attacked = true
