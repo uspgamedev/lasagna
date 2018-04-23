ee6be2d9cd191e02f2bbf943cb27f3426c2b34d2
josealvim <jose.alvim@usp.com>
2018-04-23 03:14:52 -0300

    fix interaction with merchant

--- game/character/merchant/merchant_interaction.gd
+++ game/character/merchant/merchant_interaction.gd
@@ -8,10 +8,12 @@ func interact(item):
 	var price = main.get_db().get_item_by_name(my_product).buy_value
 	if item == null:
 		if inv.money >=  price:
-			inv.give_items(my_product, 1)
-			inv.money -= price
+			if inv.give_items(my_product, 1):
+				inv.money -= price
+			else:
+				print("no slots")
 		else:
 			print("https://www.youtube.com/watch?v=rrYnZ7ZxRe4")
 	else:
-		inv.consume_n_items(item, 1)
-		inv.money += main.get_db().get_item_by_name(item).buy_value/2
+		inv.consume_n_items(item.name, 1)
+		inv.money += item.buy_value/2
