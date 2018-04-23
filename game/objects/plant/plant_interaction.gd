1013170d8beaa890e549e171ea9ffc15375fa210
Kazuo256 <kazuo256@gmail.com>
2018-04-23 11:42:57 -0300

    Don't explode with crops

--- game/objects/plant/plant_interaction.gd
+++ game/objects/plant/plant_interaction.gd
@@ -3,7 +3,8 @@ extends "res://interaction/interaction.gd"
 func interact(item):
 	var par = get_parent()
 	if par.growth_level == par.growth_steps:
-		get_node("../Sprite/AnimationPlayer").play("harvest")
+		print("HARVEST OHM OHM OHM")
+		#get_node("../Sprite/AnimationPlayer").play("harvest")
 		#HARVEST CODE
 		#...
 	else:
