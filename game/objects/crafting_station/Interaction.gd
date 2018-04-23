e51654bcabdfd5c0fcc572c2fe26fc2f15f9b317
josealvim <jose.alvim@usp.com>
2018-04-23 18:11:35 -0300

    make oven complain about cooking without a necronomicon

--- game/objects/crafting_station/Interaction.gd
+++ game/objects/crafting_station/Interaction.gd
@@ -1,4 +1,8 @@
 extends Node
 
 func interact(unused):
-	get_parent().interact(null)
+	var main = get_node("/root/Main")
+	if main.get_flags().get_flag("GOT_NECRONOMICON"):
+		get_parent().interact(null)
+	else:
+		main.execute_cutscene("cant_cook_without_recipebook")
