9c9a06be723dcf6a1f0af290b2cf522d4c820e9f
Kazuo256 <kazuo256@gmail.com>
2018-04-23 14:36:16 -0300

    Improve popup layout

--- game/objects/props/bed/bed_interaction.gd
+++ game/objects/props/bed/bed_interaction.gd
@@ -6,12 +6,12 @@ var player
 func _ready():
 	hud = get_node("/root/Main").get_hud().get_node("BedPopup")
 	hud.clear()
-	hud.add_item("Sleep")
-	hud.add_item("Don't Sleep")
+	hud.add_item(" Sleep")
+	hud.add_item(" Don't Sleep")
 
 func interact(unused):
 	if $Timer.is_stopped():
 		$Timer.start()
-		hud.popup_centered(Vector2(130, 80))
+		hud.popup_centered(Vector2(128, 64))
 		get_tree().paused = true
 	
