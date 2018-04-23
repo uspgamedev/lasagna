7a19817532cc85fb3153d832cf98803cf172c547
Kazuo256 <kazuo256@gmail.com>
2018-04-23 14:44:20 -0300

    Yieldable popup

--- game/objects/props/bed/bed_interaction.gd
+++ game/objects/props/bed/bed_interaction.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+const SLEEP = 0
+
 var hud
 var player
 
 func _ready():
-	hud = get_node("/root/Main").get_hud().get_node("BedPopup")
+	hud = get_node("/root/Main").get_hud().get_node("Popup")
 	hud.clear()
 	hud.add_item(" Sleep")
 	hud.add_item(" Don't Sleep")
@@ -14,4 +16,11 @@ func interact(unused):
 		$Timer.start()
 		hud.popup_centered(Vector2(128, 64))
 		get_tree().paused = true
+		var id = yield(hud, "id_pressed")
+		if id == SLEEP:
+			var main = get_node("/root/Main")
+			main.get_player().hp = main.get_player().max_health
+			main.sleep_and_save()
+			main.end_night()
+		get_tree().paused = false
 	
