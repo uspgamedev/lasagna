667e0e8a28232d8823b4ad4628b21b843e580e96
josealvim <jose.alvim@usp.com>
2018-04-23 20:21:15 -0300

    stabilize quests

--- game/objects/props/cupboard/Interaction.gd
+++ game/objects/props/cupboard/Interaction.gd
@@ -11,6 +11,5 @@ func interact(unused):
 		main.get_inventory().give_items("Milk",1)
 		main.get_inventory().give_items("Ham",1)
 		main.get_inventory().give_items("KepalaSeed",1)
-		main.get_inventory().give_items("MinyakSeed",1)
 	else:
 		get_node("/root/Main").execute_cutscene("cupboard_already_got_ingreds")
