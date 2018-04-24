21ae23970374ffde600fb9f59ef2698a61de83a3
orenjiakira <orenjiakira@gmail.com>
2018-04-23 21:25:30 -0300

    Fix werewolf code

--- game/character/creatures/werewolf/AI.gd
+++ game/character/creatures/werewolf/AI.gd
@@ -6,7 +6,7 @@ func get_direction():
 	return (get_node("/root/Main").get_player().position - get_parent().position).normalized()
 
 func _process(delta):
-	for body in get_node("../AggroBox").get_overlapping_bodies():
+	for body in get_node("../AgroBox").get_overlapping_bodies():
 		if body.name == "Player":
 			get_parent().get_node("SFX/Attack").play()
 			body.damage(10, get_parent())
