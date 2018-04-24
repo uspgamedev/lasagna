ad90edf88ff2bc12c0d9f1096d9ac941ebf6308b
rilifon <pudimdefrutas@gmail.com>
2018-04-23 22:09:18 -0300

    Should work sorry folks

---
+++ game/intro/intro.gd
@@ -0,0 +1,47 @@
+extends Node
+
+onready var count = 0
+onready var ini = 13
+onready var fin = 27
+onready var sfx_count_value = rand_range(ini, fin)
+
+func _ready():
+	var nana = $"Nanas-text"
+	nana.set_visible_characters(0)
+	$Timer.start()
+
+func start_text():
+	var nana = $"Nanas-text"
+	var tween = $Tween
+	var dur = 20
+	tween.interpolate_property(nana, "visible_characters", 0, nana.get_bbcode().length(), dur, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+	
+
+func _on_Tween_tween_step( object, key, elapsed, value ):
+	count = count + 1
+	if count >= sfx_count_value:
+		sfx_count_value = rand_range(ini, fin)
+		count = 0
+		var ran = randf()
+		if ran < .1:
+			$SFX.set_bus("Master-Pitchlow")
+			$SFX2.set_bus("Master-Pitchlow")
+		elif ran < .9:
+			$SFX.set_bus("Master-Pitch")
+			$SFX2.set_bus("Master-Pitch")
+		else:
+			$SFX.set_bus("Master-Pitchhigh")
+			$SFX2.set_bus("Master-Pitchhigh")
+		if not $SFX.is_playing():
+			$SFX.play()
+		else:
+			$SFX2.play()
+
+
+func _on_Tween_tween_completed( object, key ):
+	$"Begin-Game".start()
+
+
+func _begin_game():
+	print("Fazer jogo começar aqui")
