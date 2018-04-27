ec69768b8db896bec737cd5b41f155e308c8de50
Kazuo256 <kazuo256@gmail.com>
2018-04-27 17:58:11 -0300

    Add missing cutscene before ending

--- game/character/player/Consume.gd
+++ game/character/player/Consume.gd
@@ -4,26 +4,27 @@ func _ready():
 	pass
 
 func consume(item):
-	var inv = get_node("/root/Main").get_inventory()
+	var main = get_node("/root/Main")
+	var inv = main.get_inventory()
 	inv.consume_n_items(item.name, 1)
 	match item.name:
 		"HealPotion":
-			var player = get_node("/root/Main").get_player()
+			var player = main.get_player()
 			var heal = 50 # TODO: check value
 			if player.hp + heal > player.max_health:
 				player.hp = player.max_health
 			else:
 				player.hp += heal
 		"BadPotion":
-			var player = get_node("/root/Main").get_player()
+			var player = main.get_player()
 			var heal = -50 # TODO: check value
 			if player.hp + heal < 0:
 				player.hp = 0
 			else:
 				player.hp += heal
 		"LaaZahna":
-			var main = get_node("/root/Main")
 			main.get_flags().set_flag("ATE_LASAGNA", true)
 			main.execute_cutscene("eating_lasagna")
 		"WormRoll":
+			yield(main.execute_cutscene("eating_wormroll"), "finished")
 			get_tree().change_scene("res://credits/credits.tscn")
